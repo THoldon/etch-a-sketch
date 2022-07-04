316426fe3c22e07a3a21fa1d1ebac17cf209d697
THoldon <HTHAM001@e.ntu.edu.sg>
2022-07-04 20:00:30 +0800

    gridSquares working for 16x16 only, gridColor mouse not working well

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 const sixteen = document.querySelector('#sixteen');
 
-function gridSquares(){
+
+function gridSquares(){ //create gridSquares
     let i = 0;
-    for (i=0;i<16;1++) {
-        const grid = doucment.createElement('div');
+    for (i=0;i<255;i++) {
+        const grid = document.createElement('div');
+        grid.setAttribute("id","grid");
+        grid.style.borderStyle = "solid";
         grid.style.borderColor = "black";
         sixteen.appendChild(grid);
     }
 }
+gridSquares();
+
+
+
+function gridColor(){
+    const grid = document.querySelectorAll('#grid'); //add eventListener to every grid
+    var mouseDown = false;
+
+
+    grid.forEach(e =>{
+        e.addEventListener('mousedown', () =>{
+            {
+                mouseDown = true;
+            }
+            
+        e.addEventListener('mouseup', ()=>{
+            mouseDown = false;
+        })
+        })
+    })
+
+    grid.forEach(e =>{
+        e.addEventListener('mouseover', () => {
+            if(mouseDown == true)
+                e.style.backgroundColor = "pink";
+        })
+    }) 
+}
+
+gridColor();
