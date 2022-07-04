783b2ff08149854e750e22d9ebd3d20cba780911
THoldon <HTHAM001@e.ntu.edu.sg>
2022-07-04 16:45:37 +0800

    fixed spelling error

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function gridSquares(){
     let i = 0;
     for (i=0;i<16;1++) {
         const grid = doucment.createElement('div');
+        grid.style.borderColor = "black";
         sixteen.appendChild(grid);
-
     }
 }
