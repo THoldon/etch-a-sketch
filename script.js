7fc03e1dfdaa98cf398c8fa15423170ca2d63b6e
THoldon <HTHAM001@e.ntu.edu.sg>
2022-07-04 16:42:05 +0800

    html base added

---
+++ script.js
@@ -0,0 +1,10 @@
+const sixteen = document.querySelector('#sixteen');
+
+function gridSquares(){
+    let i = 0;
+    for (i=0;i<16;1++) {
+        const grid = doucment.createElement('div');
+        sixteen.appendChild(grid);
+
+    }
+}
--- script.jss
+++
@@ -1,3 +0,0 @@
-function gridSquares{
-    let i = 0;
-}
